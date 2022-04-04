f61e437d39c8723a21f8b0c1ec27bb84adb8aee4
UnChatNoir1 <87830785+UnChatNoir1@users.noreply.github.com>
2022-04-04 19:43:37 -0300

    avance

--- clases/partido.php
+++ clases/partido.php
@@ -5,14 +5,30 @@ class Partido {
     private $pais2;
     private $idPartido;
 
-    public function getid() {
-        return $this->id;
+    public function getPais1() {
+        return $this->pais1;
     }
 
-    public function setEmail($email){
-        $this->email=$email;
+    public function setPais1($pais1){
+        $this->email=$pais1;
     }
-    
+
+    public function getPais2() {
+        return $this->pais2;
+    }
+
+    public function setPais2($pais2){
+        $this->pais2=$pais2;
+    }
+
+    public function getIdPartido() {
+        return $this->idPartido;
+    }
+
+    public function setIdPartido($idPartido){
+        $this->idPartido=$idPartido;
+    }
+
 }
 
 ?>
