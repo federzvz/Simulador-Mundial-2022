d4f5892161e395846c53286208d67d0dc1911706
UnChatNoir1 <87830785+UnChatNoir1@users.noreply.github.com>
2022-04-04 19:23:48 -0300

    clase partido

---
+++ clases/partido.php
@@ -0,0 +1,18 @@
+<?php
+
+class Partido {
+    private $pais1;
+    private $pais2;
+    private $idPartido;
+
+    public function getid() {
+        return $this->id;
+    }
+
+    public function setEmail($email){
+        $this->email=$email;
+    }
+    
+}
+
+?>
