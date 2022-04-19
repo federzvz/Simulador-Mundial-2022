f1c735e69280cbe44cc32f815bb71d6af5548c42
DieterPreuss <87830546+DieterPreuss@users.noreply.github.com>
2022-04-19 19:00:53 -0300

    Update index

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once('clases/template.php');
 $controlIndex=new ControladorIndex();
 
 $tpl = Template::getInstance();
-$tpl->asignar('url_base',"http://localhost/php/framework/");
+$tpl->asignar('url_base',"http://localhost/php/Simulador-Mundial-2022/");
 $tpl->asignar('url_logout',$controlIndex->getUrl("usuario","logout"));
 $tpl->asignar('proyecto',"Apps Web");
 $tpl->asignar('url_peliculas',$controlIndex->getUrl("peliculas","listado"));
